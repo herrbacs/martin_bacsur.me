dd4176e42a58e3290f14a66e1c0622f18a53b67f
Martin Bacsur <mbacsur@sunilium.com>
2021-12-15 11:19:01 +0100

    CV Dynamic Text Content Implemented

--- src/components/CV/CV.tsx
+++ src/components/CV/CV.tsx
@@ -2,56 +2,42 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { useEffect, useState } from "react";
 import "./CV.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { LoadingModal } from "../SupportComponents/LoadingModal/LoadingModal";
+import { text } from "@fortawesome/fontawesome-svg-core";
 
 const CV = (props: any) => {
   const [textContent, setTextContent] = useState<any>(null);
-
   useEffect(() => {
     axios
       .get(`${process.env.PUBLIC_URL}/cv/cv_text_content.json`)
       .then(async (res) => {
-        await setTextContent(res.data);
-        //TODO Delete clg
-
-        const selectedLanguage = await props.global.languageSetting;
-        console.log(selectedLanguage === "eng" ? res.data.eng : res.data.hun);
-        setTextContent(
-          selectedLanguage === "eng" ? res.data.eng : res.data.hun
+        console.log(res.data.hun.technologySkills);
+        await setTextContent(
+          props.global.languageSetting === "eng" ? res.data.eng : res.data.hun
         );
       });
-  }, []);
-
+  }, [props.global.languageSetting]);
   return (
-    <div style={{ display: "flex", flexDirection: "row", height: '100%'}}>
+    <div style={{ display: "flex", flexDirection: "row", height: "100%" }}>
       {textContent ? (
         <>
           <div className={"personal-informations"}>
             <div className="me-picture-holder">
-              <img src={`${process.env.PUBLIC_URL}/cv/me.png`} alt="" />
+              <img
+                src={`${process.env.PUBLIC_URL}/cv/me.png`}
+                alt={
+                  props.global.languageSetting === "eng"
+                    ? "A picture of Martin Bacsur"
+                    : "Egy kép Bacsur Martin-ról"
+                }
+              />
             </div>
             <div className="arrow-decoration"></div>
             <div className="about-me">
-              <h3>Rólam</h3>
-              <p>
-                Szenvedélyes szegedi fejlesztőként imádok web technológiákkal
-                foglalkozni, alig várom hogy minden nap fejlődjek bennük.
-              </p>
-              <p>
-                Nagyon szeretek csapatban dolgozni, de egyedül is helytállok a
-                kihívásokkal szemben.
-              </p>
-              <p>
-                Szeretem ahogy a terveket követve valami alakul a kezem alatt,
-                de ha a váratlan keresztül húzza a számításokat, akkor sem esem
-                kétségbe.
-              </p>
-              <p>
-                Produktív munkavégzést szemelőt tartva, bátran kérek segítséget
-                csapattársaimtól, kollégáimtól.
-              </p>
+              <h3>{textContent.aboutMe.title}</h3>
+              {textContent.aboutMe.content.map((p: string, i: number) => {
+                return <p key={i}>{p}</p>;
+              })}
             </div>
             <div className="skills"></div>
           </div>
@@ -61,70 +47,79 @@ const CV = (props: any) => {
               <h4>{textContent.title}</h4>
             </div>
             <div className="work-experiencee">
-              <h3>szakmai tapasztalatok</h3>
-              <span>Szeged Sunilium Kft. :</span>
-              <span>Szakmai gyakorlat - full stack .NET fejlesztő</span>
-              <span>Egyéni Projektek: </span>
-              <span>www.docutrend.hu</span>
+              <h3>{textContent.workExperience.inJobExperience.title}</h3>
+              {textContent.workExperience.inJobExperience.content.map(
+                (exp: string, i: number) => {
+                  return (
+                    <span key={i} className={i % 2 !== 0 ? "company" : "job"}>
+                      {exp}
+                    </span>
+                  );
+                }
+              )}
+
+              <h3>{textContent.workExperience.firstHandExperience.title}</h3>
+              {textContent.workExperience.firstHandExperience.content.map(
+                (exp: string, i: number) => {
+                  return <span key={i}>{exp}</span>;
+                }
+              )}
             </div>
             <div className="technology-skills">
-              <h3>ismeretek</h3>
+              <h3>{textContent.technologySkills}</h3>
               <div className="front-end">
                 <h3>front-end</h3>
 
                 <img
                   src={`${process.env.PUBLIC_URL}/cv/frontend/js.png`}
-                  alt=""
+                  alt="Javascript"
                 />
                 <img
                   src={`${process.env.PUBLIC_URL}/cv/frontend/typescript.png`}
-                  alt=""
+                  alt="Typescript"
                 />
                 <img
                   src={`${process.env.PUBLIC_URL}/cv/frontend/react.png`}
-                  alt=""
+                  alt="ReactJs"
                 />
                 <img
                   src={`${process.env.PUBLIC_URL}/cv/frontend/jquery.png`}
-                  alt=""
+                  alt="jQuery"
                 />
                 <img
                   src={`${process.env.PUBLIC_URL}/cv/frontend/Sass.svg`}
-                  alt=""
+                  alt="Sass Script"
                 />
               </div>
               <div className="back-end">
                 <h3>back-end</h3>
                 <img
                   src={`${process.env.PUBLIC_URL}/cv/backend/node.png`}
-                  alt=""
+                  alt="Node"
                 />
                 <img
                   src={`${process.env.PUBLIC_URL}/cv/backend/netCore.png`}
-                  alt=""
+                  alt="dotNetCore"
                 />
                 <img
                   src={`${process.env.PUBLIC_URL}/cv/backend/mysql.png`}
-                  alt=""
+                  alt="mySql"
                 />
               </div>
             </div>
             <div className="education">
-              <h3>2019-2021</h3>
-              <span>
-                Szegedi Tudományegyetem - Juhász Gyula Pedagógusképző Kar
-              </span>
+              <h3>{textContent.education.title}</h3>
+              <h3>{textContent.education.first.date}</h3>
+              <span>{textContent.education.first.school}</span>
+              <span>{textContent.education.first.faculty}</span>
             </div>
             <div className="language">
-              <h3>back-end</h3>
-              <span>B2 Angol - Középfok/Általános kommunikáció szint</span>
-              <span>Programtervező Informatikus Fejlesztő</span>
-            </div>
-            <div className="hobbies">
-              <span>Motorozás</span>
-              <span>Sportolás</span>
-              <span>Közös Időtöltés Családdal, Barátokkal</span>
-              <span>Olvasás, Önfejlesztés</span>
+              <h3>{textContent.language.title}</h3>
+              {textContent.language.content.map(
+                (lang:any) => {
+                  return (<span>{lang}</span>)
+                }
+              )}
             </div>
           </div>
         </>
